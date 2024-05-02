c99254ee0a51401818bfc0a2f761dd7f2368b009
Dakota Cookenmaster <dakotacookenmaster@southern.edu>
2024-05-02 13:32:21 -0400

    Started config for UI testing

---
+++ src/apps/vsfs/components/Bitmap.test.tsx
@@ -0,0 +1,9 @@
+import { describe, test, expect, vi } from "vitest"
+import { render } from "@testing-library/react"
+import Bitmap from "./Bitmap"
+
+describe("it should render the appropriate number of pages of boxes, each appropriately numbered and colored", () => { 
+    test("256 or less should render on a single page", () => {
+        render(<Bitmap data="0000" type="inode"/>)
+    })
+})
